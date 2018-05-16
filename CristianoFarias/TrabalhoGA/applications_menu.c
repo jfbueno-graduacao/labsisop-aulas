5add4f509c477c6dfac770a48e6d5b8952dda8f8
cristianoccf <cristiano@ccfdeveloper.com>
2018-05-16 18:20:20 -0300

    Fixed

--- CristianoFarias/TrabalhoGA/applications_menu.c
+++ CristianoFarias/TrabalhoGA/applications_menu.c
@@ -1,44 +1,136 @@
+/*
+ Cristiano Costa Farias;
+ * 
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<string.h>
 #include<signal.h>
 #include<sys/types.h>
-char url[300];
-void sinal_chegou(int signum){		
-	printf("O sinal checou! \n\%s",url);
-	switch(signum){				
-		case 2:									
-			execlp("firefox","firefox --new-window", url, NULL); 
-		break;
-		default:
-			printf("Recebido sinal não catalogado: %d\n", signum);
-		break;
+
+
+pid_t op1 = -1;
+pid_t op2 = -1;
+pid_t op3 = -1;
+
+int menu() {
+    int op = -1;
+    printf("<<<< Applications Menu >>>\n\t1) Web Browser\t\t\t(executando, pid=%d)\n"
+            "\t2) Text Editor\t\t\t(executando, pid=%d)\n\t3) Terminal\t\t\t(falhou)\n"
+            "\t4) Finalizar processo\t\t(concluído)\n\t5) Quit\n"
+            "Opção: ", op1, op2);
+    scanf("%i", &op);
+    return op;
 }
+
+int webBrowser(char *url) {
+    execlp("firefox", "firefox --new-window", url, NULL);
+    return 1;
 }
 
-int main(int argc, char *W[]){
-	if(argc == 2){
-	strcat(url,W[1]);
-	struct sigaction sa;
-	memset(&sa, 0, sizeof(sa));
+int textEditor(char *name) {
+    //Iniciar editor de texto aqui.
+    printf("Falta iniciar editor de texto! ");
+    return 1;
+}
 
-	sa.sa_handler = &sinal_chegou;
+int terminal() {
+    //Iniciar terminal aqui.
+    printf("Falta iniciar terminal!");
+    return 1;
+}
 
-	printf("Meu PID = %d\n", getpid());
+int finalizar() {
+    int opScan = 0;
+    while (opScan != 4) {
+        printf("-> Opção 1: WebBrowser\n-> Opção 2: Text Editor\n-> Opção 3: Terminal\n-> Opção 4: Sair\n");
+        printf("Digite o codigo do processo que será encerrado: ");
+        scanf("%i", &opScan);
+        switch (opScan) {
+            case 1:
+                if (op1 != -1) {
+                    kill(op1, 15);
+                    op1 = -1;
+                } else {
+                    printf("Este processo não esta em execução\n");
+                }
+                break;
+            case 2:
+                if (op2 != -1) {
+                    kill(op2, 15);
+                    op2 = -1;
+                } else {
+                    printf("Este processo não esta em execução\n");
+                }
+                break;
+            case 3:
+                if (op3 != -1) {
+                    kill(op3, 15);
+                    op3 = -1;
+                } else {
+                    printf("Este processo não esta em execução\n");
+                }
+                break;
+            case 4:
+                break;
+            default:
+                printf("\nOpção inválida\n");
+                break;
+        }
+
+    }
+    return 0;
+
+}
+
+/*Inicia os chamados do menu*/
+int controller() {
+    while (1) {
+        int op = menu();
+        pid_t pid;
+        //if (check(pid) == 0) {
+        if (op == 1) {
+            char url[300];
+            printf("Digite uma URL -> ");
+            scanf("%s", url);
+            if ((pid = fork()) == 0) {
+                webBrowser(url);
+                exit(13);
+            } else if (pid > 0) {
+                op1 = pid;
+            }
+        } else if (op == 2) {
+            char name[100];
+            printf("Digite um nome de arquivo -> ");
+            scanf("%s", name);
+            if ((pid = fork()) == 0) {
+                textEditor(name);
+                exit(13);
+            } else if (pid > 0) {
+                op2 = pid;
+            }
+        } else if (op == 3) {
+            //falta terminal
+        } else if (op == 4) {
+            finalizar();
+        } else if (op == 5) {
+            exit(13);
+        } else {
+            printf("\nOpção invalida!\n");
+        }
+        //} else {
+
+        //}
+    }
+}
+
+/*Metodo principal*/
+int main(int argc, char *W[]) {
 
 
-	if(sigaction(SIGINT, &sa, NULL) != 0){
-		perror("Falha ao instalar tratador de sinal");
-		exit(-1);
-	}
-
-	for(;;)	{}	//espera (busy-waiting) pelo sinal
-	return 0;
-	}else{
-	printf("Quantidade de parâmetros informados é inválido, Digite apenas 1 parametro.\n");	
-	return 1;	
-	}	
+    controller();
 }
 
 
