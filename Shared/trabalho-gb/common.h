a9a99caecec79d1a8cf12a24a3fe0ef5acbf0e47
jfbueno <buenojeferson@outlook.com>
2018-06-21 01:09:20 -0300

    criacao do arquivo de log

--- Shared/trabalho-gb/common.h
+++ Shared/trabalho-gb/common.h
@@ -8,15 +8,22 @@ typedef struct MensagemRequisicao {
 	int codigo;
 } TMensagem;
 
+// Códigos das mensagens
+// 1 - Login, efetuado - Esperando inicio do jogo
+// 2 - Jogada inválida - Fazer nova jogada
+// 3 - Empate
+// 4 - Jogador venceu
+// 5 - Jogador perdeu
+
 typedef struct Jogada {
 	int x;
 	int y;
 } TJogada;
 
 // Nome da fila para comunicação server -> client 
-const char* FILA_SERVER_CLI = "/fila_server-cli_";
+const char* FILA_SERVER_CLI = "/fila_server-cli6";
 // Nome da fila para comunicação client -> server
-const char* FILA_CLI_SERVER = "/fila_cli-server_";
+const char* FILA_CLI_SERVER = "/fila_cli-server6";
 
 ssize_t get_msg_buffer_size(mqd_t queue) {
 	struct mq_attr attr;
